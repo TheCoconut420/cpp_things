861eb213a212f2a4c57719ad70e521185aa4e66b
Stefan The Coconut <57968327+TheCoconut420@users.noreply.github.com>
2023-02-09 18:30:59 +0100

    hello

--- Registration/registration.cpp
+++ Registration/registration.cpp
@@ -2,6 +2,20 @@
 #include <fstream>
 using namespace std;
 
+string registration() {
+    string username;
+    string password;
+
+    cout << "Please enter your Username you want to use: ";
+    cin >> username;
+
+    cout << "Please enter your Password you want to have: ";
+    cin >> password;
+}
+
+bool ch
+
+
 int main() {
     ofstream MyFile("data.txt");
 
--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
-
 using namespace std;
 
 class TicTacToe{
     public:
-        string play_board[4][4] = {
-            {"/", " 1 ", " 2 ", " 3" },
-            {"1", " - ", " - ", " - "},
-            {"2", " - ", " - ", " - "},
-            {"3", " - ", " - ", " - "},
+        // generates the board
+        string play_board[3][3] = {
+            {" - ", " - ", " - "},
+            {" - ", " - ", " - "},
+            {" - ", " - ", " - "},
         };
-        int player = 2;
+        int player = 1;
         string placement;
     
     void get_board() {
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
+        // printing the board row by row
+        cout << "   1  2  3" << endl;
+        for (int i = 0; i < 3; i++) {
+            cout << i+1 << " ";
+            for (int j = 0; j < 3; j++) {
                 cout << play_board[i][j];
             }
             cout << "\n";
@@ -23,6 +25,7 @@ class TicTacToe{
     };
 
     void set_tick() {
+        // switching the player
         if (player == 1) {
             placement = " X ";
             player = 2;
@@ -35,69 +38,64 @@ class TicTacToe{
     void place() {
         int horizontal;
         int vertical;
-        bool i;
-        while (i != true) {
+        bool legal_placement;
+        while (!legal_placement) {
             cout << "Where do you want to place your mark horizontal?: ";
             cin >> vertical;
             cout << "Where do you want to place your mark vertical?: ";
             cin >> horizontal;
             cout << "\n";
-            i = check_placement(horizontal, vertical);
+            legal_placement = check_placement(horizontal-1, vertical-1);
         }
         set_tick();
-        play_board[horizontal][vertical] = placement;
-
+        play_board[horizontal-1][vertical-1] = placement;
     };
 
     bool check_placement(int hori, int verti) {
         if (play_board[hori][verti] != " - ") {
-            cout << "Your placement was illigal! Please reenter your placement." << endl;
+            cout << "Your placement was illegal! Please re-enter your placement." << endl;
             return false;
         } else {
             return true;
         }
-
     };
 
+    public:
     bool check_player_win() {
-        if (play_board[1][1] == play_board[1][2] && play_board[1][2] == play_board[1][3] && play_board[1][1] != " - ") {
-            cout << "Player " << player << " wins!" << endl;
-            return true;
-        } else if (play_board[2][1] == play_board[2][2] && play_board[2][2] == play_board[2][3] && play_board[2][1] != " - ") {
-            cout << "Player " << player << " wins!" << endl;
-            return true;
-        } else if (play_board[3][1] == play_board[3][2] && play_board[3][2] == play_board[3][3] && play_board[3][1] != " - ") {
-            cout << "Player " << player << " wins!" << endl;
-            return true;
-        } else if (play_board[1][1] == play_board[2][1] && play_board[2][1] == play_board[3][1] && play_board[1][1] != " - ") {
-            cout << "Player " << player << " wins!" << endl;
-            return true;
-        } else if (play_board[1][2] == play_board[2][2] && play_board[2][2] == play_board[3][2] && play_board[1][2] != " - ") {
-            cout << "Player " << player << " wins!" << endl;
-            return true;
-        } else if (play_board[1][3] == play_board[2][3] && play_board[2][3] == play_board[3][3] && play_board[1][3] != " - ") {
-            cout << "Player " << player << " wins!" << endl;
-            return true;
-        } else if (play_board[1][1] == play_board[2][2] && play_board[2][2] == play_board[3][3] && play_board[1][1] != " - ") {
-            cout << "Player " << player << " wins!" << endl;
-            return true;
-        } else if (play_board[1][3] == play_board[2][2] && play_board[2][2] == play_board[3][1] && play_board[1][3] != " - ") {
-            cout << "Player " << player << " wins!" << endl;
-            return true;
-        } else {
-            return false;
+        // check rows
+        for (int i = 0; i < 3; i++) {
+            if (play_board[i][0] == play_board[i][1] && play_board[i][1] == play_board[i][2] && play_board[i][0] != " - ") {
+                cout << "Player " << player << " wins!" << endl;
+                return true;
+            }
         }
+        // check columns
+        for (int i = 0; i < 3; i++) {
+            if (play_board[0][i] == play_board[1][i] && play_board[1][i] == play_board[2][i] && play_board[0][i] != " - ") {
+                cout << "Player " << player << " wins!" << endl;
+                return true;
+            }   
+        }
+        // check diagonals
+        if (play_board[0][0] == play_board[1][1] && play_board[1][1] == play_board[2][2] && play_board[0][0] != " - ") {
+            cout << "Player " << player << " wins!" << endl;
+            return true;
+        }
+        if (play_board[0][2] == play_board[1][1] && play_board[1][1] == play_board[2][0] && play_board[0][2] != " - ") {
+            cout << "Player " << player << " wins!" << endl;
+            return true;
+        }
+
+        return false;
     };
 };
 
 int main() {
     TicTacToe game;
-    bool win;
-    while (win != true) {
+    bool stop = false;
+    while (!stop) {
         game.get_board();
         game.place();
-        win = game.check_player_win();
+        stop = game.check_player_win();
     }
-    game.get_board();
-    return 0;
 }
